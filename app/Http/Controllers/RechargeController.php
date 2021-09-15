0b667a57149abd2f22540ab4034f0865e14fe7ed
sheikhnayan <sheikhnayan1997@gmail.com>
2021-09-15 16:01:29 +0600

    asdf

--- app/Http/Controllers/RechargeController.php
+++ app/Http/Controllers/RechargeController.php
@@ -499,10 +499,11 @@ class RechargeController extends Controller
             'verify' => false,
             'body' => $xml2              
         ]);
-        return $recharge_request;
 
         $body2 = $recharge_request->getBody(); 
         $xml2 = simplexml_load_string($body2);
+
+        return $body2;
 
         $reseller_commission = ($xml2->AMOUNT/100)*a::user()->recharge;
         $admin_commission = ($xml2->AMOUNT/100)*a::user()->admin_recharge_commission;
