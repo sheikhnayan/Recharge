1841e6f12d40b9b8b35f3a407622930f3b026fc4
sheikhnayan <sheikhnayan1997@gmail.com>
2021-09-21 08:27:11 +0600

    hgfhjgf

--- app/Http/Controllers/RechargeController.php
+++ app/Http/Controllers/RechargeController.php
@@ -448,7 +448,6 @@ class RechargeController extends Controller
             $amount = $datas['amount'];
         }
         // dd($SkuCode);
-        dd($txid);
         $client = new \GuzzleHttp\Client();
             $get_amount = $client->post('https://api.dingconnect.com/api/V1/EstimatePrices',[
             'headers' => [
