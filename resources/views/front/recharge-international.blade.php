2ea1b1f8b4d33379241f9479127a91e5e3a291f7
sheikhnayan <sheikhnayan1997@gmail.com>
2021-09-21 08:35:21 +0600

    hgfhjgf

--- resources/views/front/recharge-international.blade.php
+++ resources/views/front/recharge-international.blade.php
@@ -111,7 +111,8 @@
                               <input type="hidden" name="Sku_Code" value="{{ $prods['0']['SkuCode'] }}" id="skucode">
                               <input type="hidden" id="admin_com" value="{{ Auth::user()->admin_international_recharge_commission }}">
                               <input type="hidden" id="reseller_com" value="{{ Auth::user()->international_recharge }}">
-                              <small name="received_amount" style="font-size: 18px;text-align: center;font-weight: bold;color: red;" id="price"></small><br>
+                              <small style="font-size: 18px;text-align: center;font-weight: bold;color: red;" id="price"></small><br>
+                              <input type="hidden" name="received_amount" id="received_amount">
                            </div>
                            <label class="form-label">Service Charge in EURO</label>
                            <input type="number" step="any" name="service" class="form-control" placeholder="Enter Service Charge (Optional)">
@@ -396,6 +397,7 @@
         success:function(response){
           console.log(response.Items[0].Price.ReceiveValue);
           document.getElementById("price").innerHTML = 'You Will Receive ' + response.Items[0].Price.ReceiveValue +' ' +response.Items[0].Price.ReceiveCurrencyIso+'. ';
+          document.getElementById("received_amount").setAttribute('value',response.Items[0].Price.ReceiveValue +' ' +response.Items[0].Price.ReceiveCurrencyIso+);
         },
        });
    }
