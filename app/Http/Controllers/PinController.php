ce34fd1c301d02b2e5c28f7050b57add3c10c9fb
sheikhnayan <sheikhnayan1997@gmail.com>
2021-09-27 20:13:55 +0600

    asd

--- app/Http/Controllers/PinController.php
+++ app/Http/Controllers/PinController.php
@@ -77,7 +77,7 @@ class PinController extends Controller
         $body = $recharge_request->getBody(); 
         $xml = simplexml_load_string($body);
 
-        dd($xml->PINCREDENTIALS['PIN']);
+        dd($xml->PINCREDENTIALS->PIN);
 
 
         // $data = json_encode($bod,true);
