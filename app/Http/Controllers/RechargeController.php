99daf11ee137d85475814ae289af6874d252f1ff
sheikhnayan <sheikhnayan1997@gmail.com>
2021-09-29 13:59:55 +0600

    asd

--- app/Http/Controllers/RechargeController.php
+++ app/Http/Controllers/RechargeController.php
@@ -449,7 +449,7 @@ class RechargeController extends Controller
         }
         // dd($SkuCode);
         if (a::user()->wallet >= $SendValue) {
-            $client = new \GuzzleHttp\Client(['defaults' => [ 'exceptions' => false ]]);
+            $client = new \GuzzleHttp\Client(['http_errors' => false]);
             $recharge_request = $client->post('https://api.dingconnect.com/api/V1/SendTransfer',[
             'headers' => [
             'api_key'     => 'L3YzbaxR91u6bNsgvQVeGT',
