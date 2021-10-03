b32c0fe7a084737ee87a72a6ab1caf727e74900f
sheikhnayan <sheikhnayan1997@gmail.com>
2021-10-03 17:49:16 +0600

    asd

--- app/Http/Controllers/RechargeController.php
+++ app/Http/Controllers/RechargeController.php
@@ -599,7 +599,7 @@ class RechargeController extends Controller
 
         // Rewind the stream
         $body->rewind();
-        dd($body->getContents()); // -->The request body :)
+        dd($body); // -->The request body :)
 
         $product_responses = $recharge_request->getBody();
 
