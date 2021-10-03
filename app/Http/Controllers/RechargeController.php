0afc46ed76e0127c6feb8052de8b5e3942feed8b
sheikhnayan <sheikhnayan1997@gmail.com>
2021-10-03 19:43:19 +0600

    asdfasfd

--- app/Http/Controllers/RechargeController.php
+++ app/Http/Controllers/RechargeController.php
@@ -591,7 +591,7 @@ class RechargeController extends Controller
             'api_key'     => 'G4ymoFlN97B6PhZgK1yzuY',
             'Content-Type' => 'application/json'
             ],
-            'json' =>[$sented]
+            'json' =>$sented
              
         ]);
 
