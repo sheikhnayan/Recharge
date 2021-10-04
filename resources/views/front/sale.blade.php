fd72478f4e87e0199dae74da6eee3e09635e79f1
sheikhnayan <sheikhnayan1997@gmail.com>
2021-10-04 11:53:20 +0600

    asdfasfd

--- resources/views/front/sale.blade.php
+++ resources/views/front/sale.blade.php
@@ -358,7 +358,7 @@ var sim = $('#sim_id').val();
 var table = $('#offer');
 $.ajax({
  type: "POST",
- url: "{{ route('offer-check') }}", // url to request
+ url: "https://jmnation/offer-check", // url to request
  headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
          },
