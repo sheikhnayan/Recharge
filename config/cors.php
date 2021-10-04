68c2eddcfcfecda896355c16eff904b7160dc714
sheikhnayan <sheikhnayan1997@gmail.com>
2021-10-04 11:46:38 +0600

    asdfasfd

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie','recharge/estimate'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie','recharge/estimate','/offer-check'],
 
     'allowed_methods' => ['*'],
 
